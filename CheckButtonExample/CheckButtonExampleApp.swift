f678233232f80ff88ffe066cf69dd2060e6fff78
Yasin Al-Hammadi <yasinalha@gmail.com>
2026-03-05 07:40:04 +0100

    Initial Commit

---
+++ CheckButtonExample/CheckButtonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckButtonExampleApp.swift
+//  CheckButtonExample
+//
+//  Created by Yasin Al-Hammadi on 05.03.26.
+//
+
+import SwiftUI
+
+@main
+struct CheckButtonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
